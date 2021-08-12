e6fe1490ed9b9d8f6b2886e3de39aeb8f5763738
minnake <minna.keinonen84@gmail.com>
2021-08-12 18:49:18 +0300

    Initial commit

--- index.js
+++ index.js
@@ -3,12 +3,22 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 const Vaccination = require('./models/vaccinations')
-//const Manufacturers = require('./models/manufacturers')
+const Manufacturers = require('./models/manufacturers')
 
 
 app.use(cors())
 
+const url = process.env.MONGODB_URI
 
+console.log('connecting to', url)
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+    .then(result => {
+        console.log('connected to MongoDB')
+    })
+    .catch((error) => {
+        console.log('error connecting to MongoDB:', error.message)
+    })
 
 
 app.get('/api/vaccinations', (request, response) => {
@@ -18,25 +28,37 @@ app.get('/api/vaccinations', (request, response) => {
 })
 
 app.get('/api/antiquas', (request, response) => {
-   
-
-   res.send('<h1>Hello World!</h1>')
-
-})
-
- /*app.get('/api/solarbuddhicas', (request, response) => {
-    Manufacturers.find({}).then(solarbuddhicas => {
-        response.json(solarbuddhicas)
+   try {
+        await  Manufacturers.find({}).then(antiquas => {
+        response.json(antiquas)
     })
+      } catch (err) {
+          console.log(err)
+        // Without `exec()`, the stack trace does **not** include the
+        // calling code. Below is the stack trace:
+        //
+        // CastError: Cast to ObjectId failed for value "this is not a valid id" at path "_id" for model "band-promises"
+        //   at new CastError (/app/node_modules/mongoose/lib/error/cast.js:29:11)
+        //   at model.Query.exec (/app/node_modules/mongoose/lib/query.js:4331:21)
+        //   at model.Query.Query.then (/app/node_modules/mongoose/lib/query.js:4423:15)
+        //   at process._tickCallback (internal/process/next_tick.js:68:7)
+        err.stack;
+      }
 })
 
+/*app.get('/api/solarbuddhicas', (request, response) => {
+   Manufacturers.find({}).then(solarbuddhicas => {
+       response.json(solarbuddhicas)
+   })
+})
+ 
 app.get('/api/zerpfies', (request, response) => {
-    Manufacturers.find({}).then(zerpfies => {
-        response.json(zerpfies)
-    })
+   Manufacturers.find({}).then(zerpfies => {
+       response.json(zerpfies)
+   })
 })
-
- */
+ 
+*/
 
 
 const PORT = process.env.PORT
--- models/manufacturers.js
+++ models/manufacturers.js
@@ -1,16 +1,6 @@
 const mongoose = require('mongoose')
 
-const url = process.env.MONGODB_URI
 
-console.log('connecting to', url)
-
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
 
 const itemSchema = new mongoose.Schema({
     id: String,
